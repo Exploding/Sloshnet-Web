4b7dd3455c5fa2f10988859259083aaa768a9d3c
Rahul Gupta-Iwasaki <rahulgi@stanford.edu>
2014-09-04 22:45:01 -0700

    Initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SloshnetWeb::Application.config.secret_key_base = '63f8a0d725ff8081c6a6f3ccab044ba67bce10b8fd102336cb4463b5649758681f191578244682f0d516561c5e50e6ce92829bbbd766a96de0766ebf9ba55e75'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SloshnetWeb::Application.config.session_store :cookie_store, key: '_Sloshnet-Web_session'
