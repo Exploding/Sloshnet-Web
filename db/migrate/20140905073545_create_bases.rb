7cd4d93dc8835f9ede6440ba28e931783ed9311d
Rahul Gupta-Iwasaki <rahulgi@stanford.edu>
2014-09-07 18:00:46 -0700

    Initialized model migrations

--- db/migrate/20140905073545_create_bases.rb
+++ db/migrate/20140905073545_create_bases.rb
@@ -1,7 +1,7 @@
 class CreateBases < ActiveRecord::Migration
   def change
     create_table :bases do |t|
-
+      t.string :name
       t.timestamps
     end
   end
--- db/migrate/20140905075507_create_pumps.rb
+++ db/migrate/20140905075507_create_pumps.rb
@@ -1,7 +1,7 @@
 class CreatePumps < ActiveRecord::Migration
   def change
     create_table :pumps do |t|
-
+      t.integer :pump_num
       t.timestamps
     end
   end
