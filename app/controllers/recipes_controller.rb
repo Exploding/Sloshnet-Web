38356843875af9f38cdfb58d4347ccfca6e20b90
Rahul Gupta-Iwasaki <rahulgi@stanford.edu>
2014-09-07 23:35:19 -0700

    Recipe CRUD functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,49 @@
 class RecipesController < ApplicationController
-  def new
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new
+    end
   end
 
-  def create
-    render plain: params[:recipe].inspect
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+
+    redirect_to articles_path
   end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def index
+    render text: Recipe.all.to_json
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def show
+    recipe = Recipe.find(params[:id])
+    render text: recipe.to_json
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render :edit
+    end
+  end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:name)
+    end
 end
